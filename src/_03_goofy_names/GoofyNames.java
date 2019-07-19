c34769b340fd3a767ec6d17cc3178b52f01d09fc
League Student <league@iMac-10.attlocal.net>
2019-07-19 16:29:45 -0700

    fibonacci and goofy names

--- src/_03_goofy_names/GoofyNames.java
+++ src/_03_goofy_names/GoofyNames.java
@@ -14,34 +14,38 @@ public class GoofyNames {
 		String name = JOptionPane.showInputDialog("What is your name?");
 		// 2. Print upper case name to the console using .toUpperCase()
 		//    Run your program to see that this works.
-		JOptionPane.showMessageDialog(null, name.toUpperCase());
+		
 		// 3. Loop through each character of the name (steps 4 - 6).
 		//    HINT: Use .length() to determine the number of characters in the String.
-		for (int i = 0; i < name.length(); i++) 
+		for (int i = 0; i < name.length() - 1; i++) 
 		{
 			
-		String cha = name.substring(i);
+		String cha = name.substring(i++, name.length() );
 			// 4. Create a String variable to store the next character of the name
 			//    using .substring(start, end)
 			//    HINT: replace 'start' and 'end' to get String with character at i 
 			
 			// 5. Use MODULO to set this variable to upper case for EVEN characters
 			//    and lower case for ODD characters.
-
-			if(i%2 == 1)
+		System.out.println(cha);
+			if(i%2 == 0)
 			{
 				cha.toUpperCase();
+				System.out.println(cha);
+				
 			}
 			else
 			{
 				cha.toLowerCase();
+				System.out.println(cha);
 			}
-			// 6. ADD this String  (containing 1 char) to the goofyName String
-			JOptionPane.showMessageDialog(null, name);
+			// 6. ADD this String  (containing 1 char) to the goofyName String 
+			goofyName += cha;
+			System.out.println(cha);
 		}
 		// 7. Use pop-up to show user their Goofy name
 		
-		
+		JOptionPane.showMessageDialog(null, goofyName);
 
 	}
 }
---
+++ src/fibonnacci.java
@@ -0,0 +1,41 @@
+
+public class fibonnacci 
+{
+public static void main(String[] args) 
+{
+	int num1 = 0;
+	int num2 = 1;	
+	int num3;
+	
+	for (int i = 0; i < 12; i++) 
+	{
+		
+		System.out.println(num1);
+		num3 = num1;
+		num1 += num2;
+		num2 = num3;		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
